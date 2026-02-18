d13de2dda0d15e017363418dd257bb107a42b51b
Mohamed <mohamedalashram187@gmail.com>
2026-02-18 02:32:02 +0200

    edit a name

--- app/main.py
+++ app/main.py
@@ -30,3 +30,4 @@ app.include_router(vote.router)
 @app.get("/")
 def root():
     return {"message": " I hate ni..."}
+ 
--- app/routers/user.py
+++ app/routers/user.py
@@ -32,6 +32,6 @@ async def get_user(id: int, db: Session = Depends(get_db)):
     return user
 
 @router.get("/", response_model=List[schemas.UserOut])
-def get_posts(db: Session = Depends(get_db)):
+def get_users(db: Session = Depends(get_db)):
     users = db.query(models.User).all()
     return users
